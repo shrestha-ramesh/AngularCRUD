ab8e0d7dcea99b948c3e9f8040c13d51dd65e571
Ramesh Shrestha <shresthashare@gmail.com>
2020-08-07 10:46:15 -0500

    first form is complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular Array-CURD Application';
   msg: any="";
+  model1: any={};
+  model2: any={};
 
+  products =[
+    {name: "Rice", qty: "25", price: "1300"},
+    {name: "Soap", qty: "1", price: "130"},
+    {name: "Oil", qty: "1L", price: "120"}
+  ];
+  addProduct(){
+    this.products.push(this.model1);
+    this.model1={};
+    this.msg="Record is successfully added.......";
+  }
   clickMe(){
-    
+    this.msg= "";
   }
 }
