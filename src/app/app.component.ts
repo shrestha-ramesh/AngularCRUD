f5908fee3416dfde7cb3edd89f1dab9d37c07d3d
Ramesh Shrestha <shresthashare@gmail.com>
2020-08-07 11:19:02 -0500

    added second form below first form which read a data and display the data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,17 @@ export class AppComponent {
     {name: "Soap", qty: "1", price: "130"},
     {name: "Oil", qty: "1L", price: "120"}
   ];
+  deleteProduct(i){
+    this.products.splice(i,1);
+    this.msg="Record is successfully deleted......";
+  }
+  myValue;
+  editProduct(k){
+    this.model2.name=this.products[k].name;
+    this.model2.qty=this.products[k].qty;
+    this.model1.price=this.products[k].price;
+    this.myValue=k;
+  }
   addProduct(){
     this.products.push(this.model1);
     this.model1={};
